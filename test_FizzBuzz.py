1bc3f4e785f8ef7b5d7067b383315f3084da35d2
sraju728@gmail.com <sraju728@gmail.com>
2018-10-02 18:26:50 +0530

    Add test to check the value of each multiple of three position

--- test_FizzBuzz.py
+++ test_FizzBuzz.py
@@ -2,3 +2,11 @@ import unittest
 from main import *
 
 class FizzBuzzTests(unittest.TestCase):
+
+	def testReturnsTheValueOfEachMultipleOfThree(self):
+        response = fizz_buzz()
+        FizzCount = 3
+        for index in range(len(response)):
+            if FizzCount % 15 != 0:
+                self.assertEquals(response[FizzCount-1], 'Fizz')
+                FizzCount = FizzCount + 3
