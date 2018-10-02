74acd98912a4942a152ca42d7324a5394ecb18be
sraju728@gmail.com <sraju728@gmail.com>
2018-10-02 18:35:04 +0530

    Write code for passing the first(three multiples) testcase

--- main.py
+++ main.py
@@ -5,3 +5,5 @@ def fizz_buzz():
 	for index in range(1, 101):
         if index % 3 == 0 and index % 5 == 0:
             Fizz_Buzz_List.append('FizzBuzz')
+		elif index % 3 == 0:
+            Fizz_Buzz_List.append('Fizz')
