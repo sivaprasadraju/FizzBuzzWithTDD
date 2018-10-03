6e68234920b716c833292e0ea3d38d87f80cf47e
Siva Prasad Raju <30493045+sivaprasadraju@users.noreply.github.com>
2018-10-03 09:16:27 +0530

    Refactor the code

--- main.py
+++ main.py
@@ -3,13 +3,13 @@ def fizz_buzz():
 	Fizz_Buzz_List = []
 	
 	for index in range(1, 101):
-        if index % 3 == 0 and index % 5 == 0:
-            Fizz_Buzz_List.append('FizzBuzz')
+        	if index % 3 == 0 and index % 5 == 0:
+            		Fizz_Buzz_List.append('FizzBuzz')
 		elif index % 3 == 0:
-            Fizz_Buzz_List.append('Fizz')
+            		Fizz_Buzz_List.append('Fizz')
 		elif index % 5 == 0:
-            Fizz_Buzz_List.append('Buzz')
+            		Fizz_Buzz_List.append('Buzz')
 		else:
-            Fizz_Buzz_List.append(index)
+            		Fizz_Buzz_List.append(index)
 			
 	return Fizz_Buzz_List
